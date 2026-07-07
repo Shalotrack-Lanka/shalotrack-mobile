004e8e593e2593658a23ef6a0b1d395ec70b4bdd
nethukw <nethukzz@gmail.com>
2026-07-07 22:50:17 +0530

    Added StatusResponse.java file

---
+++ app/src/main/java/com/example/letstracklanka/StatusResponse.java
@@ -0,0 +1,15 @@
+package com.example.letstracklanka;
+
+import com.google.gson.annotations.SerializedName;
+
+public class StatusResponse {
+    @SerializedName("speed")
+    private double speed;
+
+    @SerializedName("accStatus") // Ignition on/off ද කියන එක
+    private boolean accStatus;
+
+    // Getters
+    public double getSpeed() { return speed; }
+    public boolean isAccStatus() { return accStatus; }
+}
