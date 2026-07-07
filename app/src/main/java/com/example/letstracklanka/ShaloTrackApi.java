4e2d91f3289928f110977a30f6462b5a9547fbd3
nethukw <nethukzz@gmail.com>
2026-07-07 22:47:31 +0530

    Added ShaloTrackApi.java file

---
+++ app/src/main/java/com/example/letstracklanka/ShaloTrackApi.java
@@ -0,0 +1,16 @@
+package com.example.letstracklanka;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ShaloTrackApi {
+
+    // Device ID එක දීලා දැනට තියෙන ලොකේෂන් එක ගැනීම
+    @GET("api/CurrentLocations/device/{deviceId}")
+    Call<LocationResponse> getCurrentLocation(@Path("deviceId") String deviceId);
+
+    // Device ID එක දීලා Speed, ACC Status වගේ දේවල් ගැනීම
+    @GET("api/DeviceStatus/device/{deviceId}")
+    Call<StatusResponse> getDeviceStatus(@Path("deviceId") String deviceId);
+}
