f31533f7dd845929886e7e5822beb5203c8d4233
nethukw <nethukzz@gmail.com>
2026-07-07 22:49:30 +0530

    Added LocationResponse.java file

---
+++ app/src/main/java/com/example/letstracklanka/LocationResponse.java
@@ -0,0 +1,15 @@
+package com.example.letstracklanka;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LocationResponse {
+    @SerializedName("latitude") // Swagger එකේ තියෙන නම
+    private double latitude;
+
+    @SerializedName("longitude")
+    private double longitude;
+
+    // Getters
+    public double getLatitude() { return latitude; }
+    public double getLongitude() { return longitude; }
+}
